4b1a7ae9f51ffb38d73158f54167e85d51643182
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-09-03 22:07:41 +0600

    Sort Edges problem solved

--- Sort_Edges.cpp
+++ Sort_Edges.cpp
@@ -58,80 +58,29 @@
 #include <valarray>
 #include <vector>
 
+// #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> edges;
+
+    for (int i = 0; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        edges.push_back({u, v});
+    }
+
+    sort(edges.begin(), edges.end());
+
+    for (pair<int, int> v : edges)
+    {
+        cout << v.first << " " << v.second << endl;
+    }
 
     return 0;
 }
-/*
-Problem Statement
-
-You will be given E, the number of edges. For each you will be given A and B which means there is an edge between A and B.
-
-You need to sort all edges in such a way that the edges are sorted in ascending order for A. If multiple edges have same A, then you will sort them according to B in ascending order.
-
-Input Format
-
-First line will contain E.
-Next E lines will contain A and B.
-Constraints
-
-1 <= E <= 10^5
-0 <= A, B <= 10^9
-Output Format
-
-Output all the edges in sorted order.
-
-
-Sample Input 1:
-6
-3 4
-3 2
-3 5
-1 2
-1 4
-1 5
-
-Sample Output 1:
-1 2
-1 4
-1 5
-3 2
-3 4
-3 5
-
-
-
-Sample Input 2:
-5 4
-....
----.
-..-.
---..
-....
-0 1
-3 2
-
-Sample Output 2:
-YES
-
-for this input:
-5 4
-....
----.
-..-.
---..
-....
-0 1
-3 2
-
-my expected output is :
-YES
-
-but this code I'm getting output :
-
-NO
-
-*/
